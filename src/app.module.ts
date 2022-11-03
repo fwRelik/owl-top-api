5170f175c0294ca922532a8ad9c29cb82c2661ff
fwRelik <relik.fw@gmail.com>
2022-11-03 04:08:22 +0300

    Space fixing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { PageModule } from './page/page.module';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule, PageModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthModule, ProductModule, ReviewModule, PageModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
