4ffe071c4c4795dd69b7f16e97c1dcfc7fefe893
fwRelik <denis.isaev.fw@gmail.com>
2022-11-03 19:53:54 +0300

    use ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { PageModule } from './page/page.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [AuthModule, ProductModule, ReviewModule, PageModule],
+	imports: [ConfigModule.forRoot(), AuthModule, ProductModule, ReviewModule, PageModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
